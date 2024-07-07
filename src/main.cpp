b158f118e7189dcf7d5044bb10450cab4835bf65
peterspbr <m4sh1lo@protonmail.com>
2024-07-07 16:45:15 -0300

    save keys to files

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 // Main file
 
 #include <iostream>
+#include <fstream>
+#include <gmpxx.h>
 
 #include "../include/crypto.h"
 
@@ -10,9 +12,17 @@ using namespace std;
 int main() {
     Crypto crypto;
 
-    cout << "Private key: " << crypto.genPriv() << endl;
-    cout << "Public key: " << crypto.genPub() << endl;
-    cout << endl;
+    mpz_class privKey = crypto.genPriv();
+    mpz_class pubKey  = crypto.genPub();
+
+    ofstream priv("./private.key");
+    ofstream pub("./public.key");
+
+    priv << hex << privKey;
+    pub << hex << pubKey;
+
+    priv.close();
+    pub.close();
 
     mpz_class message("123456789");
     mpz_class ciphertext = crypto.encrypt(message);
