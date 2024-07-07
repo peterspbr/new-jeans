8b1bb8ac774f88ec49b4c370fef2059608e78444
peterspbr <m4sh1lo@protonmail.com>
2024-07-07 18:19:07 -0300

    added a good book and work with strings now

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,26 @@
 #include <iostream>
 #include <fstream>
 #include <gmpxx.h>
+#include <string>
 
 #include "../include/crypto.h"
 
 using namespace std;
+
+mpz_class stringToMpz(const std::string& input) {
+    mpz_class result;
+    mpz_import(result.get_mpz_t(), input.size(), 1, sizeof(input[0]), 0, 0, input.data());
+ 
+    return result;
+}
+
+std::string mpzToString(const mpz_class& input) {
+    size_t count = (mpz_sizeinbase(input.get_mpz_t(), 2) + 7) / 8; // Number of bytes
+    std::string output(count, 0);
+    mpz_export(&output[0], &count, 1, sizeof(output[0]), 0, 0, input.get_mpz_t());
+    
+    return output;
+}
 
 int main() {
     Crypto crypto;
@@ -24,11 +40,16 @@ int main() {
     priv.close();
     pub.close();
 
-    mpz_class message("123456789");
-    mpz_class ciphertext = crypto.encrypt(message);
+    string message = "Hello, world";
+    mpz_class messageInt = stringToMpz(message);
 
-    cout << "Ciphertext: " << ciphertext << endl;
-    cout << "Plaintext: " << crypto.decrypt(ciphertext) << endl;
+    mpz_class ciphertext = crypto.encrypt(messageInt);
+
+    cout << "Ciphertext: ";
+    cout << mpzToString(ciphertext) << endl;
+
+    cout << "Plaintext: ";
+    cout << mpzToString(crypto.decrypt(ciphertext)) << endl;
 
     return 0;
 }
